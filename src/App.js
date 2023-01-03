cccaf2f558ea9706ca70233e502a5d4ca94ec27d
강인호 <nanaheui000@naver.com>
2023-01-03 18:59:34 +0900

    naver login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter, Route, Routes, Link, Switch } from "react-router-dom";
+import NaverLogin from "./NaverLogin";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="login" />
+          <NaverLogin />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
